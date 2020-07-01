cc97196e49b22a7fcb7505029039c35823cddb01
David Grajales <david.grajalesc@udea.edu.co>
2020-06-30 20:40:30 -0500

    primer commit de prueba
    Signed-off-by: David Grajales <david.grajalesc@udea.edu.co>

--- app/src/main/java/com/example/practica4equipo3/Area_Hexagono.kt
+++ app/src/main/java/com/example/practica4equipo3/Area_Hexagono.kt
@@ -7,7 +7,7 @@ class Area_Hexagono : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_area__hexagono)
-
+//commit
 
         b_calcular.setOnClickListener {
 
