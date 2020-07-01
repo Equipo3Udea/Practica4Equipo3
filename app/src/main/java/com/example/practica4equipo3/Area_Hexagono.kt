d2660920ea668a61602d919c296e46fe9990dfbd
David Grajales <david.grajalesc@udea.edu.co>
2020-06-30 20:39:06 -0500

    primer commit de prueba
    Signed-off-by: David Grajales <david.grajalesc@udea.edu.co>

---
+++ app/src/main/java/com/example/practica4equipo3/Area_Hexagono.kt
@@ -0,0 +1,24 @@
+package com.example.practica4equipo3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Area_Hexagono : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area__hexagono)
+
+
+        b_calcular.setOnClickListener {
+
+            val apotema = et_apotema.text.toString()
+            val lado = et_lado.text.toString()
+            val area: Float
+
+            area = 3 * apotema.toFloat() * lado.toFloat()
+
+            tv_respuesta.text = area.toString()
+
+        }
+    }
+}
--- app/src/main/java/com/example/practica4equipo3/MainActivity.kt
+++ app/src/main/java/com/example/practica4equipo3/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.practica4equipo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//Primer commit de prueba
